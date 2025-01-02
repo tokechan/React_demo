32c5aaa415c95510bb20d04e6e5de9c7231b4477
toketime <flatoke@gmail.com>
2025-01-02 19:15:49 +0900

    todo list done

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 export default {
     root: './',  // index.htmlがpublicフォルダにあるため
+    server: {
+        watch: {
+            usePolling: true
+        },
+        hmr: {
+            overlay: true
+        }
+    },
     build: {
       outDir: 'dist'
     }
